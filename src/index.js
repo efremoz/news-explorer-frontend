1851701fd36500fb7c413295dd4881366a99b689
Denis Efremov <efremoz@gmail.com>
2020-05-18 19:19:35 +0300

    v 0.0.7

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './index.css';
+
+const formLogin = document.querySelector('.form_login');
+const formSignup = document.querySelector('.form_signup');
+
+const popup = document.querySelector('.popup');
+const buttonClose = document.querySelector('.popup__close');
+const authButton = document.querySelector('.header__button');
+
+const buttonAuth = document.querySelector('.form__switch-auth');
+const buttonRegistration = document.querySelector('.form__switch-registration');
+
+function popupClose() {
+  popup.classList.remove('popup_is-opened')
+};
+
+function popupOpen() {
+  popup.classList.add('popup_is-opened')
+};
+
+buttonClose.addEventListener('click', popupClose);
+authButton.addEventListener('click', popupOpen);
+
+buttonAuth.addEventListener('click', () => {
+  formSignup.style.display = 'none';
+  formLogin.style.display = 'block';
+  
+});
+buttonRegistration.addEventListener('click', () => {
+  formLogin.style.display = 'none';
+  formSignup.style.display = 'block';
+});
