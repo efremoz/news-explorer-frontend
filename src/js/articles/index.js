a53e07c4974db98f1b5ce72c2d67fba36312e20e
Denis Efremov <efremoz@gmail.com>
2020-05-25 23:19:52 +0300

    v 1.0.2

--- src/js/articles/index.js
+++ src/js/articles/index.js
@@ -1,1 +1,33 @@
 import '../../pages/articles/index.css';
+
+const  header = document.querySelector('.header');
+const  body = document.querySelector('.body');
+const  buttonMenu = document.querySelector('.menu__button');
+const  blcokMenuContainerMobile = document.querySelector('.menu__items-container_mobile');
+const  blockOverlay = document.querySelector('.header__menu-overlay');
+
+
+buttonMenu.addEventListener('click', () => {
+
+  let statusMenu = document.getElementsByClassName('menu__button-close');
+
+  if(1 === statusMenu.length) {
+    header.classList.remove('header_dropdown');
+    body.classList.remove('body_is-hidden');
+    blockOverlay.style.display = 'none';
+    blcokMenuContainerMobile.style.display = 'none';
+    buttonMenu.classList.remove('menu__button-close');
+    buttonMenu.classList.remove('menu__button-close_black');
+    buttonMenu.classList.add('menu__button-open');
+    buttonMenu.classList.add('menu__button-open_black');
+  } else {
+    header.classList.add('header_dropdown');
+    body.classList.add('body_is-hidden');
+    blockOverlay.style.display = 'flex';
+    blcokMenuContainerMobile.style.display = 'flex';
+    buttonMenu.classList.remove('menu__button-open');
+    buttonMenu.classList.remove('menu__button-open_black');
+    buttonMenu.classList.add('menu__button-close');
+    buttonMenu.classList.add('menu__button-close_black');
+  }
+});
--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,14 @@ const authButton = document.querySelector('.header__button');
 const buttonAuth = document.querySelector('.form__switch-auth');
 const buttonRegistration = document.querySelector('.form__switch-registration');
 
+const  header = document.querySelector('.header');
+const  body = document.querySelector('.body');
+const  buttonMenu = document.querySelector('.menu__button');
+const  blcokMenuContainerMobile = document.querySelector('.menu__items-container_mobile');
+const  blockOverlay = document.querySelector('.header__menu-overlay');
+
+const  formInput = document.querySelector('form');
+
 function popupClose() {
   popup.classList.remove('popup_is-opened')
 };
@@ -29,4 +37,29 @@ buttonAuth.addEventListener('click', () => {
 buttonRegistration.addEventListener('click', () => {
   formLogin.style.display = 'none';
   formSignup.style.display = 'block';
+});
+
+buttonMenu.addEventListener('click', () => {
+
+  let statusMenu = document.getElementsByClassName('menu__button-close');
+
+  if(1 === statusMenu.length) {
+    header.classList.remove('header_dropdown');
+    body.classList.remove('body_is-hidden');
+    blockOverlay.style.display = 'none';
+    blcokMenuContainerMobile.style.display = 'none';
+    buttonMenu.classList.remove('menu__button-close');
+    buttonMenu.classList.remove('menu__button-close_white');
+    buttonMenu.classList.add('menu__button-open');
+    buttonMenu.classList.add('menu__button-open_white');
+  } else {
+    header.classList.add('header_dropdown');
+    body.classList.add('body_is-hidden');
+    blockOverlay.style.display = 'flex';
+    blcokMenuContainerMobile.style.display = 'flex';
+    buttonMenu.classList.remove('menu__button-open');
+    buttonMenu.classList.remove('menu__button-open_white');
+    buttonMenu.classList.add('menu__button-close');
+    buttonMenu.classList.add('menu__button-close_white');
+  }  
 });
