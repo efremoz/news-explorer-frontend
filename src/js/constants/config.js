0bae9a663199bdcd62f1bc653b7716c8e534d94d
Денис Ефремов <efremoz@gmail.com>
2020-09-23 12:20:27 +0300

    Обновил url api

--- src/js/constants/config.js
+++ src/js/constants/config.js
@@ -1,6 +1,6 @@
 const config = {
   SERVER_URL: 'https://api.app-news.tk',
-  NEWSAPI_URL: 'https://praktikum.tk/news/v2/everything',
+  NEWSAPI_URL: 'http://newsapi.org/v2/everything',
   NEWSAPI_TOKEN: '09dab21e37334c80845d5393e44fba9e',
   ESCAPE_CODE: 27,
   ONE_DAY: 24 * 60 * 60 * 1000,
